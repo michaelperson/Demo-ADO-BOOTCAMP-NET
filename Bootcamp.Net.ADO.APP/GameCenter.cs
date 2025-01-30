d1a783849f6be2812c7fc894661ebeddc9ef4317
michael person <michael.person@cognitic.be>
2025-01-30 12:19:21 +0100

    ExerciceDeconnexion

--- Bootcamp.Net.ADO.APP/GameCenter.cs
+++ Bootcamp.Net.ADO.APP/GameCenter.cs
@@ -5,22 +5,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Common;
 
 namespace Bootcamp.Net.ADO.APP
 {
     public static class GameCenter
     {
 
-        private static readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DB_NET_BOOTCAMP;Integrated Security=True;Connect Timeout=60;Encrypt=False;Trust Server Certificate=False";
+        private static readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GameCenter;Integrated Security=True;Connect Timeout=60;Encrypt=False;Trust Server Certificate=False";
 
-        public static void GetStudioByName() 
+        public static void GetStudioByName()
         {
 
             List<string> list = new List<string>();
             Console.WriteLine("Veuillez entrer le nom du studio que vous cherchez : ");
             string studioName = Console.ReadLine();
 
-            using(SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlConnection conn = new SqlConnection(connectionString))
             {
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
@@ -28,7 +29,7 @@ namespace Bootcamp.Net.ADO.APP
                     cmd.CommandType = CommandType.Text;
 
                     cmd.Parameters.AddWithValue("@name", studioName);
-   
+
 
                     conn.Open();
                     using (SqlDataReader reader = cmd.ExecuteReader())
@@ -93,8 +94,127 @@ namespace Bootcamp.Net.ADO.APP
                         Console.WriteLine("le jeu n'existe pas ou n'a pas pu être supprimé !");
                     }
                 }
-            }   
+            }
         }
 
+        public static void SelectAllDetailsUnder40()
+        {
+            using (DbConnection conn = new SqlConnection(connectionString))
+            {
+                string queryGame = @"SELECT Id, Title, Description
+                                     FROM GameUnder40
+                                     ORDER BY Title";
+                using (DbCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = queryGame;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandTimeout = 10;
+                    try
+                    {
+                        conn.Open();
+                        using (DbDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.HasRows)
+                            {
+                                while (reader.Read())
+                                {
+                                    Console.WriteLine($@"
+                                        -------------------------------------------------------
+                                               Titre : {reader["Title"]} ({reader["Id"]})     
+                                        -------------------------------------------------------
+                                         Description:
+                                         ■■■☻☻■■■
+                                         {reader["Description"] ?? "♥"}
+                                         ■■■☻☻■■■
+                                        ========================================================
+
+");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Aucunes données");
+                            }
+                        }
+
+                    }
+                    catch (DbException dbex)
+                    {
+                        Console.WriteLine($"Exception : {dbex.Message}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"C'est le bordel!!! ==> {ex.Message}");
+                    }
+                    finally 
+                    { 
+                       if(conn.State != ConnectionState.Closed)
+                            conn.Close();
+                    }
+                }
+            
+            }
+        }
+
+        public static void SelectIndependantStudio()
+        {
+            //!!!! EN MODE DECONNECTE !!!!!
+
+            string query = @"SELECT Id,  Name, City , EmployeNbr as [#]
+                             FROM studio
+                             WHERE IsIndependant =1
+                             ORDER by EmployeNbr ";
+
+            DataTable IndependantStudio = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = query;
+                    using (DbDataAdapter adapter = new SqlDataAdapter(cmd))
+                    {
+                        adapter.Fill(IndependantStudio);
+                    }
+
+                }
+                //Je consomme
+
+                DisplayResult(IndependantStudio);
+
+
+                //je ferme la connexion
+                conn.Close();
+            }
+
+
+
+        }
+
+        private static void DisplayResult(DataTable independantStudio)
+        {
+            string Header = @"
+             ╔══════╦══════╦═══╦══════════╗
+             ║ Name ║ City ║Id ║ #Employee║
+             ╠══════╬══════╬═══╬══════════╣
+";
+            Console.WriteLine(Header);
+            foreach (DataRow row in independantStudio.Rows)
+            {
+                
+                Console.WriteLine($"{row["Name"]} | {row["City"]} | {row["Id"]} | {row["#"]}"); 
+                
+            }
+        }
+        public static SqlDbType GetSqlDbType(DataColumn column)
+        {
+            SqlParameter leparam = new SqlParameter();
+            leparam.ParameterName =column.ColumnName;
+            leparam.TypeName = column.DataType.ToString();
+
+
+            return leparam.SqlDbType;
+        }
     }
+
+
 }
--- Bootcamp.Net.ADO.APP/Program.cs
+++ Bootcamp.Net.ADO.APP/Program.cs
@@ -2,16 +2,13 @@
 using Microsoft.Data.SqlClient;
 using System.Data;
 
-// cmd.ExecuteScalar -> Une seule valeur, équivalent de A1
-// cmd.ExecuteNonQuery -> Nombre de lignes affectées
-// using(SqlDataRader) -> une ou plusieurs lignes
-
-
 bool continuer = true;
 while (continuer)
 {
     Console.WriteLine("1. Afficher les Details studio par nom\n" +
                       "2. Supprimer un jeu\n" +
+                      "3. Afficher le détail de tous les jeux à moins de 40€\n" +
+                      "4. Afficher les studios indépendants et leur ville classé par le nombre d'employés" +
                       "0. Quitter");
     string choix = Console.ReadLine();
     Console.Clear();
@@ -27,6 +24,12 @@ while (continuer)
             break;
         case "2":
             GameCenter.DeleteGame();
+            break;
+        case "3":
+            GameCenter.SelectAllDetailsUnder40();
+            break;
+        case "4":
+            GameCenter.SelectIndependantStudio();
             break;
 
         default:
